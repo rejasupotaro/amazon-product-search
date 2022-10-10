13eb5b0cef3b1d04e7af654732644bd44671c90d
rejasupotaro <takiguchi0817@gmail.com>
2022-10-10 09:15:11 +0900

    Add dev tools

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+
+@task
+def lint(c):
+    """Run linters (isort and black, flake8, and mypy)."""
+    print("Running isort...")
+    c.run("poetry run isort .")
+
+    print("Running black...")
+    c.run("poetry run black .")
+
+    print("Running flake8...")
+    c.run("poetry run pflake8 src tests tasks.py")
+
+    print("Running mypy...")
+    c.run("poetry run mypy src")
+    print("Done")
--- tests/test_amazon_product_search.py
+++ tests/test_amazon_product_search.py
@@ -2,4 +2,4 @@ from amazon_product_search import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
