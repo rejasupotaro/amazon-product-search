03154ba98edf63fa4d46d3b434a4e769623a1bb3
rejasupotaro <takiguchi0817@gmail.com>
2022-10-24 11:43:41 +0900

    Make model names constants

--- src/amazon_product_search/dense_retrieval/encoder.py
+++ src/amazon_product_search/dense_retrieval/encoder.py
@@ -1,9 +1,11 @@
 from sentence_transformers import SentenceTransformer
 from torch import Tensor
 
+TOHOKU = "cl-tohoku/bert-base-japanese-v2"
+SONOISA = "sonoisa/sentence-bert-base-ja-mean-tokens-v2"
 
 class Encoder:
-    def __init__(self, model_name: str = "cl-tohoku/bert-base-japanese-v2"):
+    def __init__(self, model_name: str = TOHOKU):
         # For English
         # model_name = "cross-encoder/ms-marco-electra-base"
         # model_name = "cross-encoder/ms-marco-MiniLM-L-12-v2"
@@ -14,6 +16,7 @@ class Encoder:
         # model_name = "bertin-project/bertin-roberta-base-spanish"
 
         # For Japanese
+        # model_name = "sonoisa/sentence-bert-base-ja-mean-tokens-v2"
         # model_name = "cl-tohoku/bert-base-japanese-v2"
         # model_name = "cl-tohoku/bert-base-japanese-char-v2"
         # model_name = "nlp-waseda/roberta-large-japanese"
