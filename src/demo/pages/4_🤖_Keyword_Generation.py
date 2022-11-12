542f9c3bdfc2903d9b9d0c2c387b9775b355ca0c
rejasupotaro <takiguchi0817@gmail.com>
2022-11-12 21:09:56 +0900

    Add KeywordGeneration demo

---
+++ src/demo/pages/4_🤖_Keyword_Generation.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+
+from amazon_product_search.keyword_generator import KeywordGenerator
+from amazon_product_search.nlp.normalizer import normalize_doc
+from amazon_product_search.nlp.tokenizer import Tokenizer
+from demo.page_config import set_page_config
+from demo.utils import load_products
+
+tokenizer = Tokenizer()
+generator = KeywordGenerator()
+
+
+def draw_results(results: dict[str, list[tuple[str, float]]]):
+    rows = []
+    for result in list(zip(*results.values())):
+        row = {}
+        for method, (keyword, score) in zip(results.keys(), result):
+            row[method] = (keyword, round(score, 4))
+        rows.append(row)
+    st.write(pd.DataFrame(rows))
+
+
+def main():
+    set_page_config()
+    st.write("## Keyword Generation")
+
+    st.write("### Product Catalogue")
+    df = load_products(locale="jp", nrows=1000)
+    df = df[~df["product_description"].isna() & ~df["product_bullet_point"].isna()]
+    df = df.fillna("")
+
+    product = None
+
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_pagination(paginationAutoPageSize=True)
+    gb.configure_side_bar()
+    gb.configure_selection("single", use_checkbox=True)
+    grid_options = gb.build()
+    selected_rows = AgGrid(df, gridOptions=grid_options).selected_rows
+
+    if not selected_rows:
+        return
+
+    product = selected_rows[0]
+    del product["_selectedRowNodeInfo"]
+
+    st.write("----")
+
+    st.write("### Selected Product")
+
+    st.write("#### Title")
+    title = product["product_title"]
+    st.write(title)
+    normalized_text = normalize_doc(title)
+    st.write(generator.generate(normalized_text))
+
+    st.write("#### Description")
+    description = product["product_description"]
+    st.markdown(description, unsafe_allow_html=True)
+    normalized_text = normalize_doc(description)
+    st.write(generator.generate(normalized_text))
+
+    st.write("#### Bullet Point")
+    bullet_point = product["product_bullet_point"]
+    st.write(bullet_point)
+    normalized_text = normalize_doc(bullet_point)
+    st.write(generator.generate(normalized_text))
+
+
+if __name__ == "__main__":
+    main()
