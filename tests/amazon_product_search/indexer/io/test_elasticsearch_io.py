937bbb36f7f64979f1287c4abcc6f6589e508c8f
rejasupotaro <takiguchi0817@gmail.com>
2022-11-12 21:13:41 +0900

    Fix test_elasticsearch_io.py

--- tests/amazon_product_search/indexer/io/test_elasticsearch_io.py
+++ tests/amazon_product_search/indexer/io/test_elasticsearch_io.py
@@ -23,11 +23,12 @@ def test_bulk_called_n_times(mock_es, mock_es_client):
             (
                 pipeline
                 | beam.Create(products)
-                | WriteToElasticsearch(
-                    es_host="http://localhost:9200",
-                    index_name="products",
-                    min_batch_size=batch_size,
-                    max_batch_size=batch_size,
+                | beam.BatchElements(min_batch_size=batch_size, max_batch_size=batch_size)
+                | beam.ParDo(
+                    WriteToElasticsearch(
+                        es_host="http://localhost:9200",
+                        index_name="products",
+                    )
                 )
             )
         assert mock_es_client.call_count == math.ceil(len(products) / batch_size)
