2d0379c032f45c6e430b52e84d38187b0eb6cf54
rejasupotaro <takiguchi0817@gmail.com>
2022-12-03 13:15:21 +0900

    Add tests for KeywordExtractor

--- src/amazon_product_search/keyword_extractor.py
+++ src/amazon_product_search/keyword_extractor.py
@@ -6,28 +6,28 @@ from amazon_product_search.nlp.encoder import JA_SBERT
 
 class KeywordExtractor:
     def __init__(self):
-        self.yake = pke.unsupervised.YAKE()
-        self.position_rank = pke.unsupervised.PositionRank()
-        self.multipartite_rank = pke.unsupervised.MultipartiteRank()
-        self.keybert = KeyBERT(JA_SBERT)
+        self._yake = pke.unsupervised.YAKE()
+        self._position_rank = pke.unsupervised.PositionRank()
+        self._multipartite_rank = pke.unsupervised.MultipartiteRank()
+        self._keybert = KeyBERT(JA_SBERT)
 
     def apply_yake(self, text: str) -> list[tuple[str, float]]:
-        self.yake.load_document(input=text)
-        self.yake.candidate_selection()
-        self.yake.candidate_weighting()
-        return self.yake.get_n_best(n=10)
+        self._yake.load_document(input=text)
+        self._yake.candidate_selection()
+        self._yake.candidate_weighting()
+        return self._yake.get_n_best(n=10)
 
     def apply_position_rank(self, text: str) -> list[tuple[str, float]]:
-        self.position_rank.load_document(input=text)
-        self.position_rank.candidate_selection()
-        self.position_rank.candidate_weighting()
-        return self.position_rank.get_n_best(n=10)
+        self._position_rank.load_document(input=text)
+        self._position_rank.candidate_selection()
+        self._position_rank.candidate_weighting()
+        return self._position_rank.get_n_best(n=10)
 
     def apply_multipartite_rank(self, text: str) -> list[tuple[str, float]]:
-        self.multipartite_rank.load_document(input=text)
-        self.multipartite_rank.candidate_selection(pos={"NOUN", "PROPN", "ADJ", "NUM"})
-        self.multipartite_rank.candidate_weighting()
-        return self.multipartite_rank.get_n_best(n=10)
+        self._multipartite_rank.load_document(input=text)
+        self._multipartite_rank.candidate_selection(pos={"NOUN", "PROPN", "ADJ", "NUM"})
+        self._multipartite_rank.candidate_weighting()
+        return self._multipartite_rank.get_n_best(n=10)
 
     def apply_keybert(self, text: str) -> list[tuple[str, float]]:
-        return self.keybert.extract_keywords(text, top_n=10)
+        return self._keybert.extract_keywords(text, top_n=10)
---
+++ tests/amazon_product_search/test_keyword_extractor.py
@@ -0,0 +1,55 @@
+import pytest
+
+from amazon_product_search.keyword_extractor import KeywordExtractor
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("", set()),
+        ("Hello World", {"hello world", "hello", "world"}),
+    ],
+)
+def test_apply_yake(s, expected):
+    extractor = KeywordExtractor()
+    actual = {text for text, score in extractor.apply_yake(s)}
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("", set()),
+        ("Hello World", {"world"}),
+    ],
+)
+def test_apply_position_rank(s, expected):
+    extractor = KeywordExtractor()
+    actual = {text for text, score in extractor.apply_position_rank(s)}
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("", set()),
+        ("Hello World", {"world"}),
+    ],
+)
+def test_apply_multipartite_rank(s, expected):
+    extractor = KeywordExtractor()
+    actual = {text for text, score in extractor.apply_multipartite_rank(s)}
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "s,expected",
+    [
+        ("", set()),
+        ("Hello World", {"hello", "world"}),
+    ],
+)
+def test_apply_keybert(s, expected):
+    extractor = KeywordExtractor()
+    actual = {text for text, score in extractor.apply_keybert(s)}
+    assert actual == expected
