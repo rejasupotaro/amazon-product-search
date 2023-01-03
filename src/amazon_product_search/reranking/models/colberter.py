03bc068fd7772da6c9e90418c9a366f6fd954834
rejasupotaro <takiguchi0817@gmail.com>
2023-01-03 21:52:20 +0900

    Implement ColBERT(er)

---
+++ src/amazon_product_search/reranking/models/colberter.py
@@ -0,0 +1,76 @@
+from typing import Any, Optional
+
+import torch
+from torch import nn
+from transformers import AutoModel
+
+
+class ColBERTer(nn.Module):
+    def __init__(self, bert_model_name: str):
+        super().__init__()
+        self.bert_model = AutoModel.from_pretrained(bert_model_name)
+        trainable = False
+        for p in self.bert_model.parameters():
+            p.requires_grad = trainable
+        self.score_merger = nn.Parameter(torch.zeros(1))
+        self.compression_dim = 128
+        self.compressor = nn.Linear(self.bert_model.config.hidden_size, self.compression_dim)
+        self.stopword_reducer = nn.Linear(self.compression_dim, 1, bias=True)
+        nn.init.constant_(self.stopword_reducer.bias, 1)
+
+    def forward(self, query: dict[str, torch.Tensor], doc: dict[str, Any]):
+        query_cls_vec, query_vecs, query_mask = self.encode_query(query)
+        doc_cls_vec, doc_vecs, doc_mask, token_importance = self.encode_doc(doc)
+
+        cls_score = self.compute_cls_score(query_cls_vec, doc_cls_vec)
+        term_score = self.compute_term_score(query_vecs, query_mask, doc_vecs, doc_mask, exact_scoring_mask=None)
+
+        weight = torch.sigmoid(self.score_merger)
+        cls_score *= weight
+        term_score *= 1 - weight
+        score = cls_score + term_score
+
+        return score, cls_score, term_score
+
+    def encode_query(self, query: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        cls_vec, token_vecs, token_mask = self.encode_shared(query)
+        token_vecs = token_vecs * token_mask.unsqueeze(-1)
+        return cls_vec, token_vecs, token_mask
+
+    def encode_doc(self, doc: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        cls_vec, token_vecs, token_mask = self.encode_shared(doc)
+        token_importance = nn.functional.relu(self.stopword_reducer(token_vecs))
+        token_vecs = token_vecs * token_importance
+        token_vecs = token_vecs * token_mask.unsqueeze(-1)
+        # token_mask[token_importance.squeeze(-1) <= 0] = False
+        return cls_vec, token_vecs, token_mask, token_importance
+
+    def encode_shared(self, tokens: dict[str, torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        token_mask = tokens["attention_mask"].bool()
+        vecs = self.bert_model(input_ids=tokens["input_ids"], attention_mask=tokens["attention_mask"])[0]
+        cls_vecs = vecs[:, 0, :]
+        token_vecs = self.compressor(vecs)
+        return cls_vecs, token_vecs, token_mask
+
+    def compute_cls_score(self, query_cls_vec: torch.Tensor, doc_cls_vec: torch.Tensor) -> torch.Tensor:
+        cls_score = (query_cls_vec * doc_cls_vec).sum(dim=1)
+        return cls_score
+
+    def compute_term_score(
+        self,
+        query_vecs: torch.Tensor,
+        query_mask: torch.Tensor,
+        doc_vecs: torch.Tensor,
+        doc_mask: torch.Tensor,
+        exact_scoring_mask: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        score_per_term = torch.bmm(query_vecs, doc_vecs.transpose(2, 1))
+
+        if exact_scoring_mask is not None:
+            score_per_term[~exact_scoring_mask] = 0
+
+        score_per_term[~(doc_mask).unsqueeze(1).expand(-1, score_per_term.shape[1], -1)] = -1000
+        term_score = score_per_term.max(-1).values
+        term_score[~query_mask] = 0
+        term_score = term_score.sum(-1)
+        return term_score
--- src/amazon_product_search/reranking/reranker.py
+++ src/amazon_product_search/reranking/reranker.py
@@ -8,6 +8,7 @@ from transformers.modeling_outputs import BaseModelOutput
 
 from amazon_product_search.es.response import Result
 from amazon_product_search.nlp.encoder import JA_SBERT
+from amazon_product_search.reranking.models.colberter import ColBERTer
 
 
 class Reranker(Protocol):
@@ -28,6 +29,7 @@ class RandomReranker:
 class DotReranker:
     def __init__(self, model_name: str = JA_SBERT, batch_size: int = 8):
         self.model = AutoModel.from_pretrained(model_name)
+        self.model.eval()
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.batch_size = batch_size
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,12 +48,39 @@ class DotReranker:
         if not query or not results:
             return results
 
-        self.model.eval()
-
         with torch.no_grad():
             query_emb = self.encode([query]).repeat(len(results), 1)
             product_emb = self.encode([result.product["product_title"] for result in results])
             scores = torch.diagonal(torch.mm(query_emb, product_emb.transpose(0, 1)).to("cpu"))
+        results = [result for result, score in sorted(zip(results, scores), key=lambda e: e[1], reverse=True)]
+        return results
+
+
+class ColBERTReranker:
+    def __init__(self, model_filepath: str, bert_model_name: str = "cl-tohoku/bert-base-japanese-v2"):
+        self.colberter = ColBERTer(bert_model_name)
+        self.colberter.load_state_dict(torch.load(model_filepath))
+        self.colberter.eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
+
+    def tokenize(self, texts: list[str]) -> dict[str, torch.Tensor]:
+        return self.tokenizer(
+            texts,
+            add_special_tokens=True,
+            padding="longest",
+            truncation="longest_first",
+            # max_length=self.max_length,
+            return_attention_mask=True,
+            return_tensors="pt",
+        )
+
+    def rerank(self, query: str, results: list[Result]) -> list[Result]:
+        with torch.no_grad():
+            encoded_queries = self.tokenize([query])
+            products = [result.product["product_title"] for result in results]
+            encoded_products = self.tokenize(products)
+            scores, _, _ = self.colberter(encoded_queries, encoded_products)
+            scores = scores.numpy()
         results = [result for result, score in sorted(zip(results, scores), key=lambda e: e[1], reverse=True)]
         return results
 
@@ -65,4 +94,5 @@ def from_string(reranker_str: str) -> Reranker:
         "NoOpReranker": NoOpReranker,
         "RandomReranker": RandomReranker,
         "DotReranker": DotReranker,
+        "ColBERTReranker": ColBERTReranker,
     }[reranker_str]()
