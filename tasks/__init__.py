2ca1bd685cc04ec96ad20587800fde91fa7813ec
rejasupotaro <takiguchi0817@gmail.com>
2023-05-07 11:40:19 +0900

    Remove lint and format tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,27 +1,8 @@
-from invoke import Collection, task
+from invoke import Collection
 
 from tasks import data_tasks, demo_tasks, es_tasks, gcloud_tasks, synonyms_tasks, vespa_tasks
 
-
-@task
-def format(c):
-    print("Running ruff...")
-    c.run("poetry run ruff . --fix")
-    print("Done")
-
-
-@task
-def lint(c):
-    print("Running ruff...")
-    c.run("poetry run ruff .")
-    print("Running mypy...")
-    c.run("poetry run mypy src")
-    print("Done")
-
-
 ns = Collection()
-ns.add_task(format)
-ns.add_task(lint)
 ns.add_collection(Collection.from_module(data_tasks, name="data"))
 ns.add_collection(Collection.from_module(demo_tasks, name="demo"))
 ns.add_collection(Collection.from_module(es_tasks, name="es"))
