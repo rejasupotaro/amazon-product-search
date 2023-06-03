7b79a3ba3a35704be8eee60df1c467db11a3eb7b
rejasupotaro <takiguchi0817@gmail.com>
2023-06-03 09:36:39 +0900

    Delete KeywordExtractor from Analyzer

--- src/amazon_product_search/nlp/analyzer.py
+++ src/amazon_product_search/nlp/analyzer.py
@@ -2,14 +2,12 @@ from typing import Any, Dict
 
 from amazon_product_search.nlp.normalizer import normalize_doc
 from amazon_product_search.nlp.tokenizer import Tokenizer
-from amazon_product_search.retrieval.keyword_extractor import KeywordExtractor
 
 
 class Analyzer:
     def __init__(self, text_fields: list[str]):
         self.text_fields = text_fields
         self.tokenizer = Tokenizer()
-        self.extractor = KeywordExtractor()
 
     def _normalize(self, s: str) -> str:
         s = normalize_doc(s)
