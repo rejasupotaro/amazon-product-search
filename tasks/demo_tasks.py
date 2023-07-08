62b025bb67512aac68ba6eb9d7a7e17a10dd19e7
rejasupotaro <takiguchi0817@gmail.com>
2023-07-08 16:43:16 +0900

    Fix the path of the demo app

--- tasks/demo_tasks.py
+++ tasks/demo_tasks.py
@@ -3,19 +3,19 @@ from invoke import task
 
 @task
 def eda(c):
-    c.run("poetry run streamlit run src/amazon_product_search/demo/apps/eda/📊_Product_Catalogue.py")
+    c.run("poetry run streamlit run src/demo/apps/eda/📊_Product_Catalogue.py")
 
 
 @task
 def features(c):
-    c.run("poetry run streamlit run src/amazon_product_search/demo/apps/features/🤖_Tokenization.py")
+    c.run("poetry run streamlit run src/demo/apps/features/🤖_Tokenization.py")
 
 
 @task
 def search(c):
-    c.run("poetry run streamlit run src/amazon_product_search/demo/apps/search/🔍_Retrieval.py")
+    c.run("poetry run streamlit run src/demo/apps/search/🔍_Retrieval.py")
 
 
 @task
 def vespa(c):
-    c.run("poetry run streamlit run src/amazon_product_search/demo/apps/vespa/dev_tools.py")
+    c.run("poetry run streamlit run src/demo/apps/vespa/dev_tools.py")
