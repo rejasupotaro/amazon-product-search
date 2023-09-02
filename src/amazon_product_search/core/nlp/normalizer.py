f4b06128dd41e96c27718311461ac2a65dd9d56f
rejasupotaro <takiguchi0817@gmail.com>
2023-09-03 08:32:17 +0900

    Add comments to normalization functions

--- src/amazon_product_search/core/nlp/normalizer.py
+++ src/amazon_product_search/core/nlp/normalizer.py
@@ -46,6 +46,21 @@ def remove_extra_spaces(s: str) -> str:
 
 
 def normalize_doc(s: str) -> str:
+    """Normalize a document.
+
+    In this function, we perform the following steps:
+    1. Remove HTML tags.
+    2. Normalize unicode characters.
+    3. Convert all characters to lowercase.
+    4. Remove punctuations.
+    5. Remove extra spaces.
+
+    Args:
+        s (str): A string to process.
+
+    Returns:
+        str: The processed string.
+    """
     try:
         s = remove_html_tags(s)
         s = unicodedata.normalize("NFKC", s)
@@ -59,6 +74,23 @@ def normalize_doc(s: str) -> str:
 
 
 def normalize_query(s: str) -> str:
+    """Normalize a query.
+
+    In this function, we perform the following steps:
+    1. Normalize unicode characters.
+    2. Convert all characters to lowercase.
+    3. Remove extra spaces.
+
+    Unlike `normalize_doc`, we do not remove HTML tags and punctuations
+    because they are rarely used in queries and if they appear in queries,
+    they are likely to be meaningful.
+
+    Args:
+        s (str): A string to process.
+
+    Returns:
+        str: The processed string.
+    """
     try:
         s = unicodedata.normalize("NFKC", s)
         s = s.lower()
