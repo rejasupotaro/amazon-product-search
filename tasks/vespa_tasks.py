c8e0dc1020494fd00de7fc0fc8cefc1850faa32b
rejasupotaro <takiguchi0817@gmail.com>
2023-10-05 07:38:24 +0900

    Update schema

--- tasks/vespa_tasks.py
+++ tasks/vespa_tasks.py
@@ -14,8 +14,7 @@ from amazon_product_search_dense_retrieval.encoders import SBERTEncoder
 
 @task
 def start(c):
-    vespa_app = vespa_service.start()
-    vespa_app.application_package.to_files(VESPA_DIR)
+    vespa_service.start()
 
 
 @task
@@ -39,14 +38,18 @@ def delete_all_docs(c, schema):
 
 @task
 def search(c):
+    # https://docs.vespa.ai/en/nearest-neighbor-search-guide.html
+    # https://docs.vespa.ai/en/embedding.html
+    # https://docs.vespa.ai/en/phased-ranking.html
     client = VespaClient()
-    query_vector = SBERTEncoder(HF.JP_SLUKE_MEAN).encode(input("query: "))
+    query_str = input("query: ")
+    query_vector = SBERTEncoder(HF.JP_SLUKE_MEAN).encode(query_str)
     query_vector = [float(v) for v in query_vector]
     query = {
         "yql": "select * from sources * where ({targetHits:10}nearestNeighbor(product_vector,query_vector))",
-        "hits": 10,
         "input.query(query_vector)": query_vector,
-        "ranking.profile": "semantic_similarity",
+        "ranking.profile": "hybrid",
+        "hits": 10,
     }
     res = client.search(query)
     print(res.json)
