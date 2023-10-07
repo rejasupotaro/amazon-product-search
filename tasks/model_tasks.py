defecf3c30405694a85aa8eb83fbce10069d997e
rejasupotaro <takiguchi0817@gmail.com>
2023-10-08 07:50:31 +0900

    Quantize ONNX model

--- tasks/model_tasks.py
+++ tasks/model_tasks.py
@@ -1,45 +1,91 @@
 import onnx
 import torch
 from invoke import task
-from transformers import AutoModel, AutoTokenizer
+from onnx import ModelProto
+from onnxruntime import InferenceSession
+from onnxruntime.quantization import quantize_dynamic
+from torch import Tensor
+from transformers import AutoTokenizer, BertModel, BertPreTrainedModel
+from transformers.models.bert.configuration_bert import BertConfig
 
-from amazon_product_search.constants import HF
+from amazon_product_search.constants import HF, MODELS_DIR
+
+
+class MeanPoolingEncoderONNX(BertPreTrainedModel):
+    def __init__(self, config: BertConfig) -> None:
+        super().__init__(config)
+        self.bert = BertModel(config)
+        self.init_weights()
+
+    def forward(self, input_ids: Tensor, attention_mask: Tensor, token_type_ids: Tensor | None = None) -> Tensor:
+        last_hidden_state = self.bert(input_ids, attention_mask=attention_mask).last_hidden_state
+        attention_mask = attention_mask.unsqueeze(dim=-1)
+        last_hidden_state = last_hidden_state * attention_mask
+        mean_vector = last_hidden_state.sum(dim=1) / attention_mask.sum(dim=1)
+        return mean_vector
+
+
+def print_input_and_output_names(onnx_model: ModelProto) -> None:
+    for i, input in enumerate(onnx_model.graph.input):
+        print("[Input #{}]".format(i))
+        print(input)
+
+    for i, output in enumerate(onnx_model.graph.output):
+        print("[Output #{}]".format(i))
+        print(output)
 
 
 @task
 def export(c):
-    bert_model_name = HF.JP_SLUKE_MEAN
-    model = AutoModel.from_pretrained(bert_model_name)
-    tokenizer = AutoTokenizer.from_pretrained(bert_model_name)
-    model_name = "models/model.onnx"
+    hf_model_name = HF.JP_SLUKE_MEAN
+    torch_model = MeanPoolingEncoderONNX.from_pretrained(hf_model_name).eval()
+    tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+    model_name = "model"
+    onnx_model_filepath = f"{MODELS_DIR}/{model_name}.onnx"
+    quantized_onnx_model_filepath = f"{MODELS_DIR}/{model_name}-quantized.onnx"
 
     with torch.no_grad():
         sample_tokenized = tokenizer("dummy_text", return_tensors="pt")
 
     torch.onnx.export(
-        model,
-        tuple(sample_tokenized.values()),
-        f=model_name,
+        torch_model,
+        args=tuple(sample_tokenized.values()),
+        f=onnx_model_filepath,
         input_names=["input_ids", "attention_mask"],
-        output_names=["last_hidden_state", "pooler_output"],
+        output_names=["vector"],
         dynamic_axes={
             "input_ids": {0: "batch_size", 1: "sequence"},
             "attention_mask": {0: "batch_size", 1: "sequence"},
-            "last_hidden_state": {0: "batch_size", 1: "sequence"},
-            "pooler_output": {0: "batch_size"},
+            "vector": {0: "batch_size"},
         },
         opset_version=17,
         do_constant_folding=True,
         operator_export_type=torch.onnx.OperatorExportTypes.ONNX,
     )
 
-    onnx_model = onnx.load(model_name)
-    print("====== Inputs ======")
-    for i, input in enumerate(onnx_model.graph.input):
-        print("[Input #{}]".format(i))
-        print(input)
+    quantize_dynamic(
+        model_input=onnx_model_filepath,
+        model_output=quantized_onnx_model_filepath,
+    )
 
-    print("====== Outputs ======")
-    for i, output in enumerate(onnx_model.graph.output):
-        print("[Output #{}]".format(i))
-        print(output)
+    print(f"====== {onnx_model_filepath} ======")
+    onnx_model = onnx.load(onnx_model_filepath)
+    print_input_and_output_names(onnx_model)
+
+    print(f"====== {quantized_onnx_model_filepath} ======")
+    quantized_onnx_model = onnx.load(quantized_onnx_model_filepath)
+    print_input_and_output_names(quantized_onnx_model)
+
+    session = InferenceSession(quantized_onnx_model_filepath)
+    inputs = {
+        k: v.numpy()
+        for k, v in tokenizer(
+            "dummy_text",
+            padding=True,
+            truncation=True,
+            max_length=512,
+            return_tensors="pt",
+        ).items()
+    }
+    mean_vector = session.run(None, inputs)
+    print(mean_vector)
