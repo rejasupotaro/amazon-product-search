10cbc53446d43781e8c3ce569c8619790c405b70
rejasupotaro <takiguchi0817@gmail.com>
2023-10-21 17:13:55 +0900

    Simplify AnalyzeFn

--- src/amazon_product_search/indexing/transforms/analyze_fn.py
+++ src/amazon_product_search/indexing/transforms/analyze_fn.py
@@ -1,8 +1,9 @@
-from typing import Any, Dict, Iterator
+from typing import Any, Dict, Iterator, cast
 
 import apache_beam as beam
 
-from amazon_product_search.core.nlp.analyzer import Analyzer
+from amazon_product_search.core.nlp.normalizer import normalize_doc
+from amazon_product_search.core.nlp.tokenizers import EnglishTokenizer, JapaneseTokenizer, Tokenizer
 from amazon_product_search.core.source import Locale
 
 
@@ -12,7 +13,21 @@ class AnalyzeFn(beam.DoFn):
         self.locale = locale
 
     def setup(self) -> None:
-        self.analyzer = Analyzer(self.text_fields, self.locale)
+        self.tokenizer: Tokenizer = {
+            "us": EnglishTokenizer,
+            "jp": JapaneseTokenizer,
+        }[self.locale]()
+
+    def _normalize(self, s: str) -> str:
+        s = normalize_doc(s)
+        tokens = self.tokenizer.tokenize(s)
+        return " ".join(cast(list, tokens))
 
     def process(self, product: Dict[str, Any]) -> Iterator[Dict[str, Any]]:
-        yield self.analyzer.analyze(product)
+        for field in self.text_fields:
+            if field not in product:
+                continue
+
+            normalized_text = self._normalize(product[field])
+            product[field] = normalized_text
+        yield product
