c74621140a2fad1e5301f1e8074080b4fd277708
rejasupotaro <takiguchi0817@gmail.com>
2023-10-25 08:53:13 +0900

    Move functions to rank_fusion.py

--- src/amazon_product_search/core/retrieval/rank_fusion.py
+++ src/amazon_product_search/core/retrieval/rank_fusion.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
-from typing import Literal
+from typing import Any, Literal
+
+from amazon_product_search.core.es.response import Response, Result
+from amazon_product_search.core.retrieval.score_normalizer import min_max_scale
+from amazon_product_search.core.retrieval.weighting_strategy import DynamicWeighting, FixedWeighting
 
 
 @dataclass
@@ -10,3 +14,131 @@ class RankFusion:
     enable_score_normalization: bool = False
     rrf: bool | int = False
     weighting_strategy: Literal["fixed", "dynamic"] = "fixed"
+
+
+def _normalize_scores(response: Response) -> Response:
+    """Normalize scores in a response.
+
+    Args:
+        response (Response): A response from retrieval.
+
+    Returns:
+        Response: A response with normalized scores.
+    """
+    scores = [result.score for result in response.results]
+    normalized_scores = min_max_scale(scores, min_val=0)
+    results = [
+        Result(product=result.product, score=normalized_score)
+        for result, normalized_score in zip(response.results, normalized_scores, strict=True)
+    ]
+    return Response(results=results, total_hits=response.total_hits)
+
+
+def _rrf_scores(response: Response, k: int = 60) -> Response:
+    """Adjust scores in a response using RRF (Reciprocal Rank Fusion).
+
+    Args:
+        response (Response): A response from retrieval.
+        k (int, optional): The ranking constant. Defaults to 60.
+
+    Returns:
+        Response: A response with adjusted scores.
+    """
+    adjusted_scores = []
+    for i in range(len(response.results)):
+        adjusted_scores.append(1 / (k + i + 1))
+    results = [
+        Result(product=result.product, score=adjusted_score)
+        for result, adjusted_score in zip(response.results, adjusted_scores, strict=True)
+    ]
+    return Response(results=results, total_hits=response.total_hits)
+
+
+def _append_results(original_response: Response, alternative_response: Response, size: int) -> Response:
+    """Return a response with results from alternative_response appended to original_response.
+
+    Args:
+        original_response (Response): The original response.
+        alternative_response (Response): An alternative response to append if necessary.
+        size (int): The number of results to return.
+
+    Returns:
+        Response: The response with results from alternative_response appended to original_response.
+    """
+    results = original_response.results
+    total_hits = original_response.total_hits
+    if len(results) < size:
+        results += alternative_response.results[: size - len(results)]
+        total_hits = len(results)
+    return Response(results=results, total_hits=total_hits)
+
+
+def _merge_responses_by_score(sparse_response: Response, dense_response: Response, size: int) -> Response:
+    """Merge two responses by score.
+
+    Args:
+        sparse_response (Response): A response from sparse retrieval.
+        dense_response (Response): A response from dense retrieval.
+
+    Returns:
+        Response: A merged response.
+    """
+    id_to_product: dict[str, dict[str, Any]] = {}
+    sparse_results: dict[str, float] = {}
+    dense_results: dict[str, float] = {}
+
+    for result in sparse_response.results:
+        product_id = result.product["product_id"]
+        id_to_product[product_id] = result.product
+        sparse_results[product_id] = result.score
+    for result in dense_response.results:
+        product_id = result.product["product_id"]
+        id_to_product[product_id] = result.product
+        dense_results[product_id] = result.score
+
+    results: list[Result] = []
+    for product_id in sparse_results.keys() | dense_results.keys():
+        score = sparse_results.get(product_id, 0) + dense_results.get(product_id, 0)
+        result = Result(product=id_to_product[product_id], score=score)
+        results.append(result)
+    total_hits = max(sparse_response.total_hits, dense_response.total_hits, len(results))
+
+    results = sorted(results, key=lambda result: (result.score, result.product["product_id"]), reverse=True)[:size]
+    return Response(results=results, total_hits=total_hits)
+
+
+def fuse(
+    query: str,
+    sparse_response: Response,
+    dense_response: Response,
+    sparse_boost: float,
+    dense_boost: float,
+    rank_fusion: RankFusion,
+    size: int,
+) -> Response:
+    if rank_fusion.fusion_strategy == "append":
+        return _append_results(sparse_response, dense_response, size)
+
+    if rank_fusion.enable_score_normalization:
+        sparse_response = _normalize_scores(sparse_response)
+        dense_response = _normalize_scores(dense_response)
+    elif rank_fusion.rrf:
+        if isinstance(rank_fusion.rrf, bool):
+            sparse_response = _rrf_scores(sparse_response)
+            dense_response = _rrf_scores(dense_response)
+        else:
+            sparse_response = _rrf_scores(sparse_response, k=rank_fusion.rrf)
+            dense_response = _rrf_scores(dense_response, k=rank_fusion.rrf)
+
+    if rank_fusion.weighting_strategy:
+        weighting_strategy = (
+            FixedWeighting({"sparse": sparse_boost, "dense": dense_boost})
+            if rank_fusion.weighting_strategy == "fixed"
+            else DynamicWeighting()
+        )
+        for result in sparse_response.results:
+            result.score *= weighting_strategy.apply("sparse", query)
+        for result in dense_response.results:
+            result.score *= weighting_strategy.apply("dense", query)
+
+    return _merge_responses_by_score(sparse_response, dense_response, size)
---
+++ tests/unit/amazon_product_search/core/retrieval/test_rank_fusion.py
@@ -0,0 +1,164 @@
+import numpy as np
+import pytest
+
+from amazon_product_search.core.es.response import Response, Result
+from amazon_product_search.core.retrieval.rank_fusion import (
+    _append_results,
+    _merge_responses_by_score,
+    _normalize_scores,
+    _rrf_scores,
+)
+
+
+def _is_sorted(scores: list[float]) -> None:
+    i = 0
+    try:
+        while i < len(scores) - 1:
+            assert scores[i] >= scores[i + 1]
+            i += 1
+    except AssertionError as err:
+        raise AssertionError(f"Given scores are not sorted: {scores}") from err
+
+
+@pytest.mark.parametrize(
+    ("scores", "expected_scores"),
+    [
+        ([], []),
+        ([1], [1]),
+        ([2, 1], [1, 0.5]),
+        ([2, 1, 0], [1, 0.5, 0]),
+    ],
+)
+def test_normalize_scores(scores, expected_scores):
+    results = [Result(product={"product_id": str(i)}, score=score) for i, score in enumerate(scores)]
+    response = Response(results=results, total_hits=len(results))
+    response = _normalize_scores(response)
+    assert [result.score for result in response.results] == expected_scores
+
+
+@pytest.mark.parametrize(
+    ("scores", "expected"),
+    [
+        ([], []),
+        ([1], [1]),
+        ([2, 1], [1, 0.5]),
+        ([3, 2, 1], [1, 0.5, 0.3333]),
+    ],
+)
+def test_rrf_scores(scores, expected):
+    results = [Result(product={"product_id": str(i)}, score=score) for i, score in enumerate(scores)]
+    response = Response(results=results, total_hits=len(results))
+    response = _rrf_scores(response, k=0)
+    actual = [result.score for result in response.results]
+    assert np.allclose(actual, expected, atol=1e-04)
+
+
+@pytest.mark.parametrize(
+    ("original_results", "alternative_results", "expected_total_hits", "expected_product_ids"),
+    [
+        ([], [], 0, []),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [],
+            1,
+            ["1"],
+        ),
+        (
+            [],
+            [Result(product={"product_id": "1"}, score=0)],
+            1,
+            ["1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [Result(product={"product_id": "2"}, score=0)],
+            2,
+            ["1", "2"],
+        ),
+        (
+            [
+                Result(product={"product_id": "1"}, score=0),
+                Result(product={"product_id": "2"}, score=0),
+            ],
+            [Result(product={"product_id": "3"}, score=0)],
+            2,
+            ["1", "2"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [
+                Result(product={"product_id": "2"}, score=0),
+                Result(product={"product_id": "3"}, score=0),
+            ],
+            2,
+            ["1", "2"],
+        ),
+    ],
+)
+def test_append_results(original_results, alternative_results, expected_total_hits, expected_product_ids):
+    sparse_response = Response(results=original_results, total_hits=len(original_results))
+    dense_response = Response(results=alternative_results, total_hits=len(alternative_results))
+    response = _append_results(sparse_response, dense_response, size=2)
+    assert response.total_hits == expected_total_hits
+    assert [result.product["product_id"] for result in response.results] == expected_product_ids
+
+
+@pytest.mark.parametrize(
+    ("sparse_results", "dense_results", "expected_total_hits", "expected_product_ids"),
+    [
+        ([], [], 0, []),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [],
+            1,
+            ["1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [Result(product={"product_id": "1"}, score=0)],
+            1,
+            ["1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0), Result(product={"product_id": "2"}, score=0)],
+            [],
+            2,
+            ["2", "1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0)],
+            [Result(product={"product_id": "2"}, score=0)],
+            2,
+            ["2", "1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0), Result(product={"product_id": "2"}, score=0)],
+            [Result(product={"product_id": "1"}, score=0)],
+            2,
+            ["2", "1"],
+        ),
+        (
+            [Result(product={"product_id": "1"}, score=0), Result(product={"product_id": "2"}, score=0)],
+            [Result(product={"product_id": "3"}, score=0), Result(product={"product_id": "4"}, score=0)],
+            4,
+            ["4", "3", "2", "1"],
+        ),
+    ],
+)
+def test_merge_responses_by_score(sparse_results, dense_results, expected_total_hits, expected_product_ids):
+    sparse_response = Response(results=sparse_results, total_hits=len(sparse_results))
+    dense_response = Response(results=dense_results, total_hits=len(dense_results))
+    response = _merge_responses_by_score(sparse_response, dense_response, size=100)
+    assert response.total_hits == expected_total_hits
+    assert [result.product["product_id"] for result in response.results] == expected_product_ids
+    _is_sorted([result.score for result in response.results])
+
+
+def test_merge_responses_by_score_with_size():
+    sparse_results = [Result(product={"product_id": str(i)}, score=0) for i in range(0, 3)]
+    sparse_response = Response(results=sparse_results, total_hits=len(sparse_results))
+    dense_results = [Result(product={"product_id": str(i)}, score=0) for i in range(3, 6)]
+    dense_response = Response(results=dense_results, total_hits=len(dense_results))
+    response = _merge_responses_by_score(sparse_response, dense_response, size=4)
+    assert response.total_hits == 6
+    assert len(response.results) == 4
