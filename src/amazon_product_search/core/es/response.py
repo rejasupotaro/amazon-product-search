7da8973c18abe20c3045728ca1f314d321ba5e1a
rejasupotaro <takiguchi0817@gmail.com>
2023-11-04 17:45:49 +0900

    Parse explanation

--- src/amazon_product_search/core/es/response.py
+++ src/amazon_product_search/core/es/response.py
@@ -10,9 +10,9 @@ class Result:
 
     @staticmethod
     def _get_dense_score(explanation: dict[str, Any]) -> float:
-        if explanation["description"] != "within top k documents":
+        if explanation.get("description") != "within top k documents":
             return 0
-        return explanation["value"]
+        return explanation.get("value", 0)
 
     def get_scores_in_explanation(self) -> tuple[float, float]:
         """Get a summary of scoring.
@@ -25,12 +25,16 @@ class Result:
         if not self.explanation:
             return 0, 0
 
+        sparse_score, dense_score = self.explanation.get("sparse_score"), self.explanation.get("dense_score")
+        if sparse_score is not None and dense_score is not None:
+            return sparse_score, dense_score
+
         dense_score = self._get_dense_score(self.explanation)
         if dense_score:
             return 0, dense_score
 
         sparse_score, dense_score = self.score, 0
-        for child_explanation in self.explanation["details"]:
+        for child_explanation in self.explanation.get("details", []):
             dense_score += self._get_dense_score(child_explanation)
         return sparse_score - dense_score, dense_score
 
