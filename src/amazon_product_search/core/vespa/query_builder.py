8f2f9b2424d831a4b3c7da9338fc180af7f1de70
rejasupotaro <takiguchi0817@gmail.com>
2024-02-03 17:09:53 +0900

    Update Vespa queries

--- src/amazon_product_search/core/vespa/query_builder.py
+++ src/amazon_product_search/core/vespa/query_builder.py
@@ -28,50 +28,83 @@ class QueryBuilder:
             return query_vector
         return [float(v) for v in list(self.encoder.encode(query_str))]
 
+    @staticmethod
+    def _build_text_matching_query(tokens: list[str], fields: list[str]) -> str:
+        and_conditions = []
+        for token in tokens:
+            or_conditions = []
+            for field in fields:
+                or_conditions.append(f"{field} contains '{token}'")
+            and_conditions.append(f"({' OR '.join(or_conditions)})")
+        return " AND ".join(and_conditions)
+
     def build_query(
-        self, query_str: str, rank_profile: str, size: int, is_semantic_search_enabled: bool
+        self,
+        query_str: str,
+        rank_profile: str,
+        size: int,
+        is_semantic_search_enabled: bool,
+        fields: list[str] | None,
+        alpha: float = 0.5,
     ) -> dict[str, Any]:
         query_str = normalize_query(query_str)
         tokens = cast(list, self.tokenizer.tokenize(query_str))
         query_str = " ".join(tokens)
 
+        if not fields:
+            fields = ["default"]
+        text_matching_query = QueryBuilder._build_text_matching_query(tokens, fields)
+
         query = {
             "query": query_str,
-            "input.query(alpha)": 0.5,
+            "input.query(alpha)": alpha,
             "ranking.profile": rank_profile,
             "hits": size,
         }
         if is_semantic_search_enabled:
             query[
                 "yql"
-            ] = """
+            ] = f"""
             select
                 *
             from
                 product
             where
-                userQuery()
-                or ({targetHits:100, approximate:true}nearestNeighbor(product_vector, query_vector))
+                {text_matching_query}
+                or ({{targetHits:{size}, approximate:true}}nearestNeighbor(product_vector, query_vector))
             """
             query["input.query(query_vector)"] = self.encode(query_str)
         else:
             query[
                 "yql"
-            ] = """
+            ] = f"""
             select
                 *
             from
                 product
             where
-                userQuery()
+                {text_matching_query}
             """
         return query
 
-    def build_lexical_search_query(self, query_str: str, size: int) -> dict[str, Any]:
-        return self.build_query(query_str, rank_profile="lexical", size=size, is_semantic_search_enabled=False)
+    def build_lexical_search_query(self, query_str: str, size: int, fields: list[str] | None = None) -> dict[str, Any]:
+        return self.build_query(
+            query_str, rank_profile="lexical", size=size, is_semantic_search_enabled=False, fields=fields
+        )
 
-    def build_semantic_search_query(self, query_str: str, size: int) -> dict[str, Any]:
-        return self.build_query(query_str, rank_profile="semantic", size=size, is_semantic_search_enabled=True)
+    def build_semantic_search_query(self, query_str: str, size: int, fields: list[str] | None = None) -> dict[str, Any]:
+        return self.build_query(
+            query_str, rank_profile="semantic", size=size, is_semantic_search_enabled=True, fields=fields
+        )
 
-    def build_hybrid_search_query(self, query_str: str, size: int) -> dict[str, Any]:
-        return self.build_query(query_str, rank_profile="hybrid", size=size, is_semantic_search_enabled=True)
+    def build_hybrid_search_query(
+        self, query_str: str, size: int, fields: list[str] | None = None, alpha: float = 0.5
+    ) -> dict[str, Any]:
+        return self.build_query(
+            query_str,
+            rank_profile="hybrid",
+            size=size,
+            is_semantic_search_enabled=True,
+            fields=fields,
+            alpha=alpha,
+        )
