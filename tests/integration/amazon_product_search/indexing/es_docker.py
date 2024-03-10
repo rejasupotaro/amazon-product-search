ea926d6b60f23aa9c66688c4453e63d628aaa001
rejasupotaro <takiguchi0817@gmail.com>
2024-03-10 13:19:52 +0900

    Upgrade Elasticsearch

--- tests/integration/amazon_product_search/indexing/es_docker.py
+++ tests/integration/amazon_product_search/indexing/es_docker.py
@@ -19,7 +19,7 @@ class EsDocker:
             self.container.restart()
         except NotFound:
             self.container = docker_client.containers.run(
-                image="docker.elastic.co/elasticsearch/elasticsearch:8.10.4",
+                image="docker.elastic.co/elasticsearch/elasticsearch:8.12.1",
                 name=container_id,
                 ports={9200: 9200},
                 environment={
