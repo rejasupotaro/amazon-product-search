222d90d1693bb4ece4e4a6078666a583ca87c838
rejasupotaro <takiguchi0817@gmail.com>
2024-04-01 21:00:08 +0900

    Allow specifying product fields to encode

--- src/amazon_product_search/indexing/transforms/encode_product.py
+++ src/amazon_product_search/indexing/transforms/encode_product.py
@@ -14,15 +14,22 @@ from amazon_product_search_dense_retrieval.encoders import SBERTEncoder
 from amazon_product_search_dense_retrieval.encoders.modules.pooler import PoolingMode
 
 
-def _product_to_text(product: Dict[str, Any]) -> str:
-    return product["product_title"] + " " + product["product_brand"]
+def _product_to_text(product: Dict[str, Any], fields: list[str]) -> str:
+    return " ".join(product[field] for field in fields)
 
 
 class EncodeProductFn(beam.DoFn):
-    def __init__(self, shared_handle: Shared, hf_model_name: str, pooling_mode: PoolingMode = "mean") -> None:
+    def __init__(
+        self,
+        shared_handle: Shared,
+        hf_model_name: str,
+        product_fields: list[str],
+        pooling_mode: PoolingMode = "mean",
+    ) -> None:
         self._shared_handle = shared_handle
         self._hf_model_name = hf_model_name
         self._pooling_mode = pooling_mode
+        self._product_fields = product_fields
 
     def setup(self) -> None:
         def initialize_encoder() -> WeakReference[SBERTEncoder]:
@@ -36,7 +43,7 @@ class EncodeProductFn(beam.DoFn):
 
     def process(self, products: List[Dict[str, Any]]) -> Iterator[Tuple[str, List[float]]]:
         logging.info(f"Encode {len(products)} products in a batch")
-        texts = [_product_to_text(product) for product in products]
+        texts = [_product_to_text(product, self._product_fields) for product in products]
         product_vectors = self._encode(texts)
         for product, product_vector in zip(products, product_vectors, strict=True):
             yield product["product_id"], product_vector.tolist()
@@ -44,9 +51,14 @@ class EncodeProductFn(beam.DoFn):
 
 class EncodeProductTritonFn(beam.DoFn):
     def __init__(
-        self, hf_model_name: str, host: str = "localhost:8001", onnx_model_name: str = "text_embedding"
+        self,
+        hf_model_name: str,
+        product_fields: list[str],
+        host: str = "localhost:8001",
+        onnx_model_name: str = "text_embedding",
     ) -> None:
         self._hf_model_name = hf_model_name
+        self._product_fields = product_fields
         self._host = host
         self._onnx_model_name = onnx_model_name
 
@@ -54,7 +66,7 @@ class EncodeProductTritonFn(beam.DoFn):
         self._client = InferenceServerClient(host=self._host)
 
     def process(self, products: List[Dict[str, Any]]) -> Iterator[Tuple[str, List[float]]]:
-        texts = [_product_to_text(product) for product in products]
+        texts = [_product_to_text(product, self._product_fields) for product in products]
         product_vectors = self._client.infer(
             model_name=self._onnx_model_name,
             inputs=[
@@ -70,11 +82,21 @@ class EncodeProductTritonFn(beam.DoFn):
 
 
 class EncodeProduct(beam.PTransform):
-    def __init__(self, shared_handle: Shared, hf_model_name: str, batch_size: int, use_triton: bool = False) -> None:
+    def __init__(
+        self,
+        shared_handle: Shared,
+        hf_model_name: str,
+        batch_size: int,
+        product_fields: list[str] | None = None,
+        use_triton: bool = False,
+    ) -> None:
         super().__init__()
         self._shared_handle = shared_handle
         self._hf_model_name = hf_model_name
         self._batch_size = batch_size
+        if not product_fields:
+            product_fields = ["product_title"]
+        self._product_fields = product_fields
         self._use_triton = use_triton
 
     def expand(self, pcoll: beam.PCollection[Dict[str, Any]]) -> beam.PCollection[Dict[str, Any]]:
@@ -85,6 +107,7 @@ class EncodeProduct(beam.PTransform):
                     EncodeProductFn(
                         shared_handle=self._shared_handle,
                         hf_model_name=self._hf_model_name,
+                        product_fields=self._product_fields,
                     )
                 )
             )
@@ -93,6 +116,7 @@ class EncodeProduct(beam.PTransform):
                 beam.ParDo(
                     EncodeProductTritonFn(
                         hf_model_name=self._hf_model_name,
+                        product_fields=self._product_fields,
                     )
                 )
             )
---
+++ tests/unit/amazon_product_search/indexing/transforms/test_encode_product.py
@@ -0,0 +1,23 @@
+import pytest
+
+from amazon_product_search.indexing.transforms.encode_product import _product_to_text
+
+
+@pytest.mark.parametrize(
+    ("product", "fields", "expected"),
+    [
+        (
+            {"product_title": "product title"},
+            ["product_title"],
+            "product title",
+        ),
+        (
+            {"product_title": "product title", "product_description": "product description"},
+            ["product_title", "product_description"],
+            "product title product description",
+        ),
+    ],
+)
+def test_product_to_text(product, fields, expected):
+    actual = _product_to_text(product, fields)
+    assert actual == expected
