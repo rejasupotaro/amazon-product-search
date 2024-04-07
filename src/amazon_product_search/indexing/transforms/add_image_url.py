1761145c3ef7626dd599bdb47efec0eed107f66e
rejasupotaro <takiguchi0817@gmail.com>
2024-04-07 16:06:01 +0900

    Implement AddImageUrlFn

---
+++ src/amazon_product_search/indexing/transforms/add_image_url.py
@@ -0,0 +1,48 @@
+import os
+from typing import Any, Dict, Iterator
+
+import apache_beam as beam
+import pandas as pd
+
+from amazon_product_search.core.source import Locale
+
+
+class AddImageUrlFn(beam.DoFn):
+    """This is a Beam DoFn that adds image URL to product.
+
+    The data source is extracted from https://github.com/shuttie/esci-s?tab=readme-ov-file
+
+    Args:
+        filepath (str): Path to the parquet file that contains `asin`, `locale`, and `image_url`.
+        locale (Locale): Locale of the products.
+
+    Attributes:
+        asin_to_image_url (Dict[str, str]): A dictionary that maps ASIN to image URL.
+    """
+
+    def __init__(self, filepath: str, locale: Locale) -> None:
+        super().__init__()
+        self.asin_to_image_url = self.load_image_url_dict(filepath, locale)
+
+    @staticmethod
+    def load_image_url_dict(filepath: str, locale: Locale) -> Dict[str, str]:
+        if not os.path.exists(filepath):
+            # Return an empty dictionary if the file does not exist.
+            return {}
+
+        df = pd.read_parquet(filepath)
+        df = df[df["locale"] == locale]
+        return dict(zip(df["asin"], df["image_url"], strict=True))
+
+    def process(self, product: Dict[str, Any]) -> Iterator[Dict[str, Any]]:
+        """Add image URL to product.
+
+        Args:
+            product (Dict[str, Any]): _description_
+
+        Yields:
+            Iterator[Dict[str, Any]]: _description_
+        """
+        if product["product_id"] in self.asin_to_image_url:
+            product["image_url"] = self.asin_to_image_url[product["product_id"]]
+        yield product
---
+++ tests/unit/amazon_product_search/indexing/transforms/test_add_image_url.py
@@ -0,0 +1,32 @@
+import apache_beam as beam
+import pandas as pd
+from apache_beam.testing.test_pipeline import TestPipeline
+from apache_beam.testing.util import assert_that, equal_to
+
+from amazon_product_search.indexing.transforms.add_image_url import AddImageUrlFn
+
+
+def test_add_image_url(tmp_path):
+    filepath = tmp_path / "product_images.parquet"
+    pd.DataFrame(
+        [
+            {
+                "asin": "2",
+                "locale": "us",
+                "image_url": "https://amazon.product.search/2.jpg",
+            }
+        ]
+    ).to_parquet(filepath, index=False)
+
+    products = [
+        {"product_id": "1"},
+        {"product_id": "2"},
+    ]
+    expected = [
+        {"product_id": "1"},
+        {"product_id": "2", "image_url": "https://amazon.product.search/2.jpg"},
+    ]
+
+    with TestPipeline() as pipeline:
+        actual = pipeline | beam.Create(products) | beam.ParDo(AddImageUrlFn(filepath=filepath, locale="us"))
+        assert_that(actual=actual, matcher=equal_to(expected=expected))
