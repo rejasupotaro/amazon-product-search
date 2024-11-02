f2a4c9e950456060dd9a92df42fde4834fe2b18f
rejasupotaro <takiguchi0817@gmail.com>
2024-11-02 17:17:51 +0900

    Move files

--- src/amazon-product-search/tasks/__init__.py
+++ src/amazon-product-search/tasks/__init__.py
@@ -2,7 +2,6 @@ from invoke import Collection, task
 
 from tasks import (
     data_tasks,
-    demo_tasks,
     es_tasks,
     indexing_tasks,
     model_tasks,
@@ -21,7 +20,6 @@ def verify(c):
 ns = Collection()
 ns.add_task(verify)
 ns.add_collection(Collection.from_module(data_tasks, name="data"))
-ns.add_collection(Collection.from_module(demo_tasks, name="demo"))
 ns.add_collection(Collection.from_module(es_tasks, name="es"))
 ns.add_collection(Collection.from_module(indexing_tasks, name="indexing"))
 ns.add_collection(Collection.from_module(model_tasks, name="model"))
