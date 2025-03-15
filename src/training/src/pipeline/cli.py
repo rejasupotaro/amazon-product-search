211bd8cd81917c23c767695703ee7228c056c46d
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-03-16 07:50:20 +0900

    Read config vars from env

--- src/training/src/pipeline/cli.py
+++ src/training/src/pipeline/cli.py
@@ -24,46 +24,36 @@ def greet():
 
 @app.command()
 def run(
-    project_id: Annotated[str, typer.Option()],
-    project_name: Annotated[str, typer.Option()],
-    region: Annotated[str, typer.Option()],
-    service_account: Annotated[str, typer.Option()],
-    templates_dir: Annotated[str, typer.Option()],
     training_image: Annotated[str, typer.Option()],
-    staging_bucket: Annotated[str, typer.Option()],
     pipeline_type: Annotated[str, typer.Option()],
 ) -> None:
-    experiment = f"{pipeline_type}-1"
-    pipeline_name = f"{pipeline_type}-{get_unix_timestamp()}"
-    template_path = f"{templates_dir}/{pipeline_type}.yaml"
-
     overrides = [
-        f"project_id={project_id}",
-        f"project_dir=gs://{project_name}",
         f"pipeline_type={pipeline_type}",
         f"compile_parameters.image={training_image}",
         f"runtime_parameters={pipeline_type}",
     ]
-
     config = load_config(overrides)
 
+    experiment = f"{pipeline_type}-1"
+    pipeline_name = f"{pipeline_type}-{get_unix_timestamp()}"
+
     aiplatform.init(
-        project=project_id,
-        location=region,
-        staging_bucket=staging_bucket,
+        project=config.project_id,
+        location=config.region,
+        staging_bucket=config.staging_bucket,
         experiment=experiment,
     )
 
     pipeline = PIPELINE_DICT[pipeline_type](config.compile_parameters)
-    pipeline.compile(template_path=template_path)
+    pipeline.compile(template_path=config.template_filepath)
 
     job = aiplatform.PipelineJob(
         display_name=pipeline_name,
-        template_path=template_path,
+        template_path=config.template_filepath,
         parameter_values=config.runtime_parameters,
     )
     job.submit(
-        service_account=service_account,
+        service_account=config.service_account,
         experiment=experiment,
     )
     job._block_until_complete()
