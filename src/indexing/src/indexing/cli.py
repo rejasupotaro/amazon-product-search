10a0ede889104309fa37d978300cf279bd5d743e
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-03-20 08:03:55 +0900

    Add task: transform

--- src/indexing/src/indexing/cli.py
+++ src/indexing/src/indexing/cli.py
@@ -1,13 +1,23 @@
 import contextlib
+import subprocess
 
 import typer
 from elasticsearch import NotFoundError
+from hydra import compose, initialize
+from omegaconf import DictConfig, OmegaConf
 from typing_extensions import Annotated
 
 from amazon_product_search.constants import HF
 from amazon_product_search.es.es_client import EsClient
 
 app = typer.Typer()
+
+
+def load_config(config_name: str, overrides: list[str]) -> DictConfig:
+    with initialize(config_path="../../conf"):
+        config= compose(config_name=config_name, overrides=overrides)
+    OmegaConf.resolve(config)
+    return config
 
 
 @app.command()
@@ -37,3 +47,53 @@ def import_model(
 ) -> None:
     es_client = EsClient()
     es_client.import_model(model_id=HF.JA_SBERT)
+
+
+@app.command()
+def transform(
+) -> None:
+    config = load_config("transform", [])
+
+    command = [
+        "poetry run python src/indexing/doc_pipeline.py",
+        f"--locale={config.locale}",
+        f"--index_name={config.index_name}",
+        f"--runner={config.runner}",
+        f"--dest={config.dest}",
+        f"--dest_host={config.dest_host}",
+    ]
+
+    if config.encode_text:
+        command.append("--encode_text")
+
+    if config.runner == "DirectRunner":
+        command += [
+            # https://github.com/apache/beam/blob/master/sdks/python/apache_beam/options/pipeline_options.py#L617-L621
+            "--direct_num_workers=0",
+        ]
+    elif config.runner == "DataflowRunner":
+        command += [
+            "--num_workers=1",
+            "--worker_machine_type=n2-highmem-8",
+            "--sdk_location=container",
+            f"--sdk_container_image=gcr.io/{config.project_id}/{config.project_name}/indexing",
+            f"--worker_zone={config.region}-c",
+        ]
+
+    if (config.runner == "DataflowRunner") or (config.dest == "bq"):
+        command += [
+            f"--project={config.project_id}",
+            f"--region={config.region}",
+            f"--temp_location=gs://{config.project_name}/temp",
+            f"--staging_location=gs://{config.project_name}/staging",
+        ]
+
+    if config.dest == "bq" and config.table_id:
+        command += [
+            f"--table_id={config.table_id}",
+        ]
+
+    if config.nrows:
+        command.append(f"--nrows={config.nrows}")
+
+    subprocess.run(command)
