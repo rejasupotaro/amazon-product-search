16da9edbe24f7fb5517c2680617f4b375d4e84ef
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-03-29 10:55:38 +0900

    Delete the original export task

--- src/amazon-product-search/tasks/__init__.py
+++ src/amazon-product-search/tasks/__init__.py
@@ -2,7 +2,6 @@ from invoke import Collection, task
 
 from tasks import (
     data_tasks,
-    model_tasks,
     synonyms_tasks,
     vespa_tasks,
 )
@@ -17,6 +16,5 @@ def verify(c):
 ns = Collection()
 ns.add_task(verify)
 ns.add_collection(Collection.from_module(data_tasks, name="data"))
-ns.add_collection(Collection.from_module(model_tasks, name="model"))
 ns.add_collection(Collection.from_module(synonyms_tasks, name="synonyms"))
 ns.add_collection(Collection.from_module(vespa_tasks, name="vespa"))
