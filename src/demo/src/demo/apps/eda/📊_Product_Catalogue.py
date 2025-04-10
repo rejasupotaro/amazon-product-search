77e129930208f99a8a1a706bafa333979bdc5186
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-04-10 20:58:45 +0900

    Migrate product catalogue

--- src/demo/src/demo/apps/eda/📊_Product_Catalogue.py
+++ src/demo/src/demo/apps/eda/📊_Product_Catalogue.py
@@ -1,45 +1,45 @@
 import plotly.express as px
-import polars as pl
 import streamlit as st
+from pandas import DataFrame
 from st_aggrid import AgGrid, GridOptionsBuilder
 
 from demo.page_config import set_page_config
 from demo.utils import analyze_dataframe, load_products
 
 
-def draw_column_info(products_df: pl.DataFrame) -> None:
+def draw_column_info(products_df: DataFrame) -> None:
     st.write("### Columns")
     analyzed_df = analyze_dataframe(products_df)
-    st.write(analyzed_df.to_pandas())
+    st.write(analyzed_df)
 
 
-def draw_brand_info(products_df: pl.DataFrame) -> None:
+def draw_brand_info(products_df: DataFrame) -> None:
     st.write("### Brand")
-    count_df = products_df.groupby("product_brand").count().sort("count", reverse=True).head(100)
+    count_df = products_df.groupby("product_brand").size().reset_index(name="count").sort_values("count", ascending=False).head(100)
 
-    fig = px.bar(count_df.to_pandas(), x="product_brand", y="count")
+    fig = px.bar(count_df, x="product_brand", y="count")
     fig.update_layout(title="Top 50 brands")
     st.plotly_chart(fig, use_container_width=True)
 
 
-def draw_color_info(products_df: pl.DataFrame) -> None:
+def draw_color_info(products_df: DataFrame) -> None:
     st.write("### Color")
-    count_df = products_df.groupby("product_color").count().sort("count", reverse=True).head(100)
+    count_df = products_df.groupby("product_color").size().reset_index(name="count").sort_values("count", ascending=False).head(100)
 
-    fig = px.bar(count_df.to_pandas(), x="product_color", y="count")
+    fig = px.bar(count_df, x="product_color", y="count")
     fig.update_layout(title="Top 50 color names")
     st.plotly_chart(fig, use_container_width=True)
 
 
-def draw_examples(products_df: pl.DataFrame) -> None:
+def draw_examples(products_df: DataFrame) -> None:
     st.write("### Examples")
 
-    gb = GridOptionsBuilder.from_dataframe(products_df.to_pandas())
+    gb = GridOptionsBuilder.from_dataframe(products_df)
     gb.configure_pagination(paginationAutoPageSize=True)
     gb.configure_side_bar()
     gb.configure_selection("single", use_checkbox=True)
     grid_options = gb.build()
-    selected_rows = AgGrid(products_df.to_pandas(), gridOptions=grid_options).selected_rows
+    selected_rows = AgGrid(products_df, gridOptions=grid_options).selected_rows
 
     if not selected_rows:
         return
