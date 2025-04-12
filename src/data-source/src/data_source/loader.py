da4aa486adf3344d8a002be12a41cf53bb812176
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-04-13 07:59:43 +0900

    Make data_dir optional

--- src/data-source/src/data_source/loader.py
+++ src/data-source/src/data_source/loader.py
@@ -6,7 +6,7 @@ import pandas as pd
 Locale = Literal["us", "es", "jp"]
 
 
-def find_data_dir() -> Path:
+def find_data_dir() -> str:
     """Find the data directory for the package.
 
     If I want to make it more robust to changes, I could use
@@ -22,17 +22,13 @@ def find_data_dir() -> Path:
     However, this is a simple solution that works for the current structure of the package.
     """
     package_root = Path(__file__).resolve().parents[2]
-    return package_root / "data"
+    return str(package_root / "data")
 
 
-
-
-
-
-
-def load_examples(locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame:
-    data_dir = find_data_dir()
-    examples_df = pd.read_parquet(data_dir / "shopping_queries_dataset_examples.parquet")
+def load_examples(locale: Locale | None = None, nrows: int = -1, data_dir: str | None = None) -> pd.DataFrame:
+    if data_dir is None:
+        data_dir = find_data_dir()
+    examples_df = pd.read_parquet(f"{data_dir}/shopping_queries_dataset_examples.parquet")
     if locale is not None:
         examples_df = examples_df[examples_df["product_locale"] == locale]
     if nrows > 0:
@@ -40,7 +36,9 @@ def load_examples(locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame
     return examples_df
 
 
-def load_products(data_dir: str, locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame:
+def load_products(locale: Locale | None = None, nrows: int = -1, data_dir: str | None = None) -> pd.DataFrame:
+    if data_dir is None:
+        data_dir = find_data_dir()
     products_df = pd.read_parquet(f"{data_dir}/shopping_queries_dataset_products.parquet")
     if locale is not None:
         products_df = products_df[products_df["product_locale"] == locale]
@@ -49,14 +47,16 @@ def load_products(data_dir: str, locale: Locale | None = None, nrows: int = -1) 
     return products_df
 
 
-def load_sources(data_dir: str) -> pd.DataFrame:
+def load_sources(data_dir: str | None = None) -> pd.DataFrame:
+    if data_dir is None:
+        data_dir = find_data_dir()
     sources_df = pd.read_csv(f"{data_dir}/shopping_queries_dataset_sources.csv")
     return sources_df
 
 
-def load_merged(data_dir: str, locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame:
-    examples_df = load_examples(data_dir, locale)
-    products_df = load_products(data_dir, locale)
+def load_merged(locale: Locale | None = None, nrows: int = -1, data_dir: str | None = None) -> pd.DataFrame:
+    examples_df = load_examples(locale=locale, data_dir=data_dir)
+    products_df = load_products(locale=locale, data_dir=data_dir)
     merged_df = examples_df.merge(
         products_df[["product_id", "product_title"]],
         on="product_id",
