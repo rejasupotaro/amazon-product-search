61e921aa2497514f8d3db7aa3314f414d3728fc6
Kentaro Takiguchi <kentaro-takiguchi@mercari.com>
2025-04-13 07:56:17 +0900

    Implement find_data_dir

--- src/data-source/src/data_source/loader.py
+++ src/data-source/src/data_source/loader.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Literal
 
 import pandas as pd
@@ -5,8 +6,33 @@ import pandas as pd
 Locale = Literal["us", "es", "jp"]
 
 
-def load_examples(data_dir: str, locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame:
-    examples_df = pd.read_parquet(f"{data_dir}/shopping_queries_dataset_examples.parquet")
+def find_data_dir() -> Path:
+    """Find the data directory for the package.
+
+    If I want to make it more robust to changes, I could use
+    ```
+    current_path = Path(__file__).resolve()
+    for parent in current_path.parents:
+        data_dir = parent / "data"
+        if data_dir.exists():
+            return data_dir
+    raise FileNotFoundError("Data directory not found.")
+    ```
+
+    However, this is a simple solution that works for the current structure of the package.
+    """
+    package_root = Path(__file__).resolve().parents[2]
+    return package_root / "data"
+
+
+
+
+
+
+
+def load_examples(locale: Locale | None = None, nrows: int = -1) -> pd.DataFrame:
+    data_dir = find_data_dir()
+    examples_df = pd.read_parquet(data_dir / "shopping_queries_dataset_examples.parquet")
     if locale is not None:
         examples_df = examples_df[examples_df["product_locale"] == locale]
     if nrows > 0:
